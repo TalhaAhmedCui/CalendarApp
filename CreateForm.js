03f39999694da7d3ebdab01dbd743a0634da6e39
nbiscan <nbiscan7@gmail.com>
2019-04-05 11:23:25 +0200

    Create event with custom dates.

--- CreateForm.js
+++ CreateForm.js
@@ -1,43 +1,123 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Text, Alert } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import { Button } from "react-native-elements";
 import { styles } from "./styles";
 import axios from "axios";
+import DatePicker from "react-native-datepicker";
+
 const moment = require("moment");
 
 class CreateForm extends Component {
-  state = { text: "Enter title" };
+  static navigationOptions = {
+    title: "Create event"
+  };
+
+  state = {
+    text: "Enter title",
+    start: moment().format("YYYY-MM-DD hh:mm"),
+    end: moment().format("YYYY-MM-DD hh:mm")
+  };
 
   async create() {
     const { navigation } = this.props;
     const token = navigation.getParam("accessToken");
     const email = navigation.getParam("email");
 
+    const { text, start, end } = this.state;
+
+    const startSplitted = start.toString().split(" ");
+    const endSplitted = end.toString().split(" ");
+
     const resp = await axios.post(
       `https://www.googleapis.com/calendar/v3/calendars/${email}/events?access_token=${token}`,
       {
         start: {
-          dateTime: "2019-04-04T09:30:00.0z"
+          dateTime: `${startSplitted[0]}T${startSplitted[1]}:00.0Z` //`2019-04-04T09:30:00.0z`
         },
         end: {
-          dateTime: "2019-04-04T10:30:00.0z"
+          dateTime: `${endSplitted[0]}T${endSplitted[1]}:00.0Z` //"2019-04-04T10:30:00.0z"
         },
-        summary: this.state.text
+        summary: text
       }
     );
 
-    console.log(resp.data);
+    //console.log(resp.data);
+
+    if (resp.status === 200) {
+      navigation.goBack();
+    } else {
+      Alert.alert("Error, please try again");
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <Text style={styles.header}>Create new event:</Text>
+        <Text>Enter summary</Text>
         <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          style={{
+            height: 40,
+            width: 400,
+            borderColor: "gray",
+            borderWidth: 1
+          }}
           onChangeText={text => this.setState({ text })}
           value={this.state.text}
         />
+        <Text>Enter start date</Text>
+        <DatePicker
+          style={{ width: 400 }}
+          date={this.state.start}
+          mode="datetime"
+          placeholder="select date"
+          format="YYYY-MM-DD hh:mm"
+          is24Hour={true}
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              position: "absolute",
+              left: 0,
+              top: 4,
+              marginLeft: 0
+            },
+            dateInput: {
+              marginLeft: 36
+            }
+          }}
+          onDateChange={start => {
+            this.setState({ start });
+          }}
+        />
+
+        <Text>Enter end date</Text>
+        <DatePicker
+          style={{ width: 400 }}
+          date={this.state.end}
+          mode="datetime"
+          placeholder="select date"
+          format="YYYY-MM-DD hh:mm"
+          is24Hour={true}
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              position: "absolute",
+              left: 0,
+              top: 4,
+              marginLeft: 0
+            },
+            dateInput: {
+              marginLeft: 36
+            }
+          }}
+          onDateChange={end => {
+            this.setState({ end });
+          }}
+        />
+
         <Button title="Submit" onPress={() => this.create()} />
       </View>
     );
